51e9aff45738aa9446c0889fa37fc14406cfa845
道玄文集自动构建 <daoxuan-bot@github.com>
2026-07-17 17:04:03 +0000

    🔄 自动构建部署 #10 aac272493cea1741fef256f23831825736165eef

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@
  * 道玄文集 - Service Worker
  * ==========================
  * 功能：首次访问后缓存所有静态资源，后续访问秒开
- * 版本：daoxuan-v1784307030  （构建时自动替换）
- * 构建时间：2026-07-17T16:50:30.964Z
+ * 版本：daoxuan-v1784307842  （构建时自动替换）
+ * 构建时间：2026-07-17T17:04:02.174Z
  */
 
-const CACHE_NAME = 'daoxuan-v1784307030';
+const CACHE_NAME = 'daoxuan-v1784307842';
 const STATIC_ASSETS = [
     './',
     './index.html',
